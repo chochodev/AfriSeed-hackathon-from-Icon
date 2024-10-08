b83cf77fe8118f1e1ca0fa5020951849f3a2dfee
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-08 15:07:07 +0100

    update: done with hero section for now

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <MainLayout>
-      <div className='flex size-full justify-center py-[4rem] lg:py-[6rem] px-[1rem] '>
+      <div className='flex size-full justify-center py-[4rem] md:py-[6rem] px-[1rem] bg-neutral-100 '>
         <div className='flex flex-col md:items-center w-full max-w-[72rem] space-y-[2rem] md:text-center'>
-          <h1 className='text-[3rem] lg:text-[3.25rem] font-[700]'>Fuel Africa&apos;s Innovation 
+          <h1 className='text-[2.5rem] sm:text-[2.875rem] smd:text-[3rem] lg:text-[3.25rem] font-[700]'>Fuel Africa&apos;s Innovation 
             <p className='bg-gradient-to-r from-primary-600 via-purple-600 to-red-500 text-transparent bg-clip-text text-clip'>Support Local Entrepreneurs Today</p>
           </h1>
           <p className='text-[0.875rem] lg:text-[1.15rem] text-neutral-800 max-w-[50rem] mx-auto '>Support Local African entrepreneurs and ignite a wave of innovation that transforms lives. Your contribution empowers small businesses, creating lasting change in communities. Together, we can fuel Africa's future today!</p>
