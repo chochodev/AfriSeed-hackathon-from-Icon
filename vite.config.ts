fc8899fb67249bf228173a4799b8cd772d6c0210
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-08 12:31:46 +0100

    update: css with tailwindcss working normally

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,11 @@ export default defineConfig({
     alias: {
       "$": "/src"
     },
-  }
+  },
+    
+  css: {
+    postcss: {
+      plugins: [tailwindcss('./tailwind.config.js')],
+    },
+  },
 })
