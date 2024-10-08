272b313f34f2257b56971634fef5e77b441e79c2
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-08 13:10:18 +0100

    update: done with navbar desktop view

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -7,16 +7,16 @@ const Navbar = () => {
       <nav className='flex place-items-center gap-x-[2rem] w-full max-w-[72rem] h-[4.5rem] '>
         <img src='/assets/vite.svg' alt='LOGO' className='' />
 
-        <div className='flex items-center space-x-[2rem] h-full '>
-          
-            <Link 
-              to='/investors'
-              className='flex items-center h-full text-center hover:text-primary-600 border-solid border-0 hover:border-primary-600 hover:border-b-[2px] ease-250'
-            >Investors</Link>
-            <Link 
-              to='/business'
-              className='flex items-center h-full text-center hover:text-primary-600 border-solid border-0 hover:border-primary-600 hover:border-b-[2px] ease-250'
-            >Business</Link>
+        <nav className='flex flex-1 items-center space-x-[2rem] h-full w-full '>
+          {/* ::::::::::::::::::::::: nav links */}
+          <Link 
+            to='/investors'
+            className='flex items-center h-full text-center hover:text-primary-600 border-solid border-0 border-b-2 border-transparent hover:border-primary-600 ease-250'
+          >Investors</Link>
+          <Link 
+            to='/business'
+            className='flex items-center h-full text-center hover:text-primary-600 border-solid border-0 border-b-2 border-transparent hover:border-primary-600 ease-250'
+          >Business</Link>
           
           <div className='relative size-max flex place-items-center '>
             <RiSearch2Line className='absolute left-[1rem] text-[1.25rem] text-neutral-500 ' />
@@ -27,7 +27,13 @@ const Navbar = () => {
             />
           </div>
           
-        </div>
+          <div className='flex justify-end flex-1'>
+            <Link 
+              to='/'
+              className='bg-primary-600 hover:bg-primary-800 active:bg-primary-500 ease-250 text-white text-[1rem] py-[0.5rem] px-[1.5rem] rounded-[1rem] '
+            >Connect Wallet</Link>
+          </div>
+        </nav>
       </nav>
     </header>
   )
