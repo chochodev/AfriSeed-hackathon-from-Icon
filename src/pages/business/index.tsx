a6f1cccdbcd50d9097e5fee099bfe9d2410c1278
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-09 21:00:06 +0100

    update: done with the business page for now

--- src/pages/business/index.tsx
+++ src/pages/business/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BusinessCard from '$/components/businessCard';
 import MainLayout from '$/layout';
 import { RiSearch2Line, RiArrowDownSLine } from "react-icons/ri";
@@ -30,6 +30,21 @@ const Business = () => {
     // }
   }
 
+  useEffect(() => {
+    const button = document.getElementById('filter-button');
+
+    if (button) {
+      button.addEventListener('blur', ()=>setIsOpen(!isOpen));
+    }
+
+    // :::::::::::::::::::: cleanup function 
+    return () => {
+      if (button) {
+        button.removeEventListener('blur', ()=>setIsOpen(!isOpen));
+      }
+    };
+  }, [isOpen]);
+
   return (
     <MainLayout>
       <div className='flex size-full flex-col space-y-[4rem] items-center justify-center py-[3rem] md:py-[6rem] px-[1rem] lg:px-[2rem] bg-neutral-100 '>
@@ -44,7 +59,7 @@ const Business = () => {
               <RiSearch2Line className='absolute left-[1rem] text-[1.25rem] text-neutral-500 ' />
               <input 
                 type='text' 
-                className='max-w-[10rem] lg:max-w-[12.5rem] h-[2.5rem] pl-[2.5rem] p-[1rem] rounded-[6.25rem] bg-white outline outline-1 outline-transparent focus:outline-neutral-300 ease-250' 
+                className='max-w-[10rem] lg:max-w-[12.5rem] h-[2.5rem] pl-[2.5rem] p-[1rem] rounded-[6.25rem] bg-white outline outline-1 outline-neutral-200 focus:outline-neutral-400 ease-250' 
                 placeholder='Search'
               />
             </div>
@@ -53,8 +68,9 @@ const Business = () => {
             <div className="w-full max-w-[10rem] space-y-4">
               <div className="relative">
                 <button
-                  className="w-full flex justify-between items-center px-4 py-2 text-left bg-white border border-neutral-300 rounded-[1rem] focus:outline-none focus:ring-[1px] focus:ring-neutral-300"
+                  className="w-full flex justify-between items-center px-4 py-2 text-left bg-white border-solid border-[1px] border-neutral-200 rounded-[1rem] focus:outline-none focus:border-neutral-400"
                   onClick={() => setIsOpen(!isOpen)}
+                  id='filter-button'
                 >
                   {options.find(option => option.value === value)?.label || "Select filter"}
                   <RiArrowDownSLine className="h-5 w-5 text-neutral-400" />
@@ -79,7 +95,9 @@ const Business = () => {
           {/* ::::::::::::::::::::::: title */}
           <h2 className='text-[2rem] font-[600] capitalize'>{value} Businesses</h2>
 
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-[0.5rem] gap-y-[2rem] lg:gap-y-[4rem] lg:gap-[2rem] py-[2rem] '>
+          <div 
+            className='grid grid-cols-1 md:grid-cols-3 gap-[0.5rem] gap-y-[2rem] lg:gap-y-[4rem] lg:gap-[2rem] py-[2rem] '
+          >
             {Array.from({length: 9}).map((_, i) => (
               <BusinessCard key={i} />
             ))}
