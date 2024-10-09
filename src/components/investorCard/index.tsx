43c4faf2be58a90de4e74beebf90940e7af1b25f
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-09 21:21:44 +0100

    update: done with the investors page for now

--- src/components/investorCard/index.tsx
+++ src/components/investorCard/index.tsx
@@ -16,28 +16,28 @@ const InvestorCard = ({
   imageUrl
 }: InvestorCardProps) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-sm">
+    <div className="group bg-white shadow-[0_2px_5px_-1px_rgba(0,0,0,0.2)] rounded-[16px] overflow-hidden max-w-sm">
       <div className="relative h-48">
         <img
           src={imageUrl}
           alt={`${name} from ${company}`}
-          className="object-cover transition-transform duration-300 ease-in-out transform hover:scale-105"
+          className="object-cover ease-250 group-hover:scale-105"
         />
       </div>
-      <div className="p-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">{name}</h2>
+      <div className="p-6 group-hover:bg-neutral-50 ease-250">
+        <h2 className="text-2xl font-bold mb-2 text-neutral-800">{name}</h2>
         <div className="flex items-center mb-4">
-          <FaBuilding className="text-gray-500 mr-2" />
-          <span className="text-gray-600">{company}</span>
+          <FaBuilding className="text-neutral-500 mr-2" />
+          <span className="text-neutral-600">{company}</span>
         </div>
         <div className="flex items-center mb-4">
           <FaDollarSign className="text-green-500 mr-2" />
-          <span className="text-gray-700 font-semibold">
+          <span className="text-neutral-700 font-semibold">
             ${investmentAmount.toLocaleString()}
           </span>
         </div>
-        <p className="text-gray-600 mb-4">{description}</p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+        <p className="text-neutral-600 mb-4">{description}</p>
+        <button className="bg-primary-600 hover:bg-primary-800 text-white font-bold py-2 px-4 rounded-[2rem] ease-250 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50">
           Contact Investor
         </button>
       </div>
--- src/pages/investors/index.tsx
+++ src/pages/investors/index.tsx
@@ -1,11 +1,118 @@
+import { useEffect, useState } from 'react';
+import InvestorCard from '$/components/investorCard';
 import MainLayout from '$/layout';
+import { RiSearch2Line, RiArrowDownSLine } from "react-icons/ri";
 
-const Investor = () => {
+// interface Item {
+//   name: string
+//   verified: boolean
+// }
+
+const options = [
+  { value: "all", label: "All" },
+  { value: "verified", label: "Verified" },
+]
+
+const Investors = () => {
+  const [value, setValue] = useState<string>("all")
+  // const [filteredItems, setFilteredItems] = useState<Item[]>(items)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleSelect = (currentValue: string) => {
+    setValue(currentValue)
+    setIsOpen(false)
+    // if (currentValue === "all") {
+    //   setFilteredItems(items)
+    // } else if (currentValue === "verified") {
+    //   setFilteredItems(items.filter(item => item.verified))
+    // } else {
+    //   setFilteredItems(items.filter(item => !item.verified))
+    // }
+  }
+
+  useEffect(() => {
+    const button = document.getElementById('filter-button');
+
+    if (button) {
+      button.addEventListener('blur', ()=>setIsOpen(!isOpen));
+    }
+
+    // :::::::::::::::::::: cleanup function 
+    return () => {
+      if (button) {
+        button.removeEventListener('blur', ()=>setIsOpen(!isOpen));
+      }
+    };
+  }, [isOpen]);
+
   return (
     <MainLayout>
-      Investor
+      <div className='flex size-full flex-col space-y-[4rem] items-center justify-center py-[3rem] md:py-[6rem] px-[1rem] lg:px-[2rem] bg-neutral-100 '>
+        <div className='w-full max-w-[72rem]'>     
+          <h2 className='text-[2rem] lg:text-[2.5rem] font-[800] '>Investment opportunities</h2>
+          <p className='text-[1rem] text-neutral-500'>Browse to contact possible investors for your business.<br/>
+          All investors are vetted & pass due diligence.</p>
+
+          <nav className='flex items-center space-x-[2rem] w-full py-[2rem] '>
+            {/* :::::::::::::::::::::::::: search */}
+            <div className='relative size-max flex place-items-center '>
+              <RiSearch2Line className='absolute left-[1rem] text-[1.25rem] text-neutral-500 ' />
+              <input 
+                type='text' 
+                className='max-w-[10rem] lg:max-w-[12.5rem] h-[2.5rem] pl-[2.5rem] p-[1rem] rounded-[6.25rem] bg-white outline outline-1 outline-neutral-200 focus:outline-neutral-400 ease-250' 
+                placeholder='Search'
+              />
+            </div>
+
+            {/* ::::::::::::::::::::::::: filter */}
+            <div className="w-full max-w-[10rem] space-y-4">
+              <div className="relative">
+                <button
+                  className="w-full flex justify-between items-center px-4 py-2 text-left bg-white border-solid border-[1px] border-neutral-200 rounded-[1rem] focus:outline-none focus:border-neutral-400"
+                  onClick={() => setIsOpen(!isOpen)}
+                  id='filter-button'
+                >
+                  {options.find(option => option.value === value)?.label || "Select filter"}
+                  <RiArrowDownSLine className="h-5 w-5 text-neutral-400" />
+                </button>
+                {isOpen && (
+                  <div className="absolute top-[calc(100%+0.5rem)] z-10 w-full bg-white border border-neutral-300 rounded-[1rem] shadow-lg outline outline-[1px] outline-neutral-300">
+                    {options.map((option) => (
+                      <button
+                        key={option.value}
+                        className="block w-full px-4 py-2 text-left hover:bg-neutral-100 focus:outline-none"
+                        onClick={() => handleSelect(option.value)}
+                      >
+                        {option.label}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+          </nav>
+
+          {/* ::::::::::::::::::::::: title */}
+          <h2 className='text-[2rem] font-[600] capitalize'>{value} Investors</h2>
+
+          <div 
+            className='grid grid-cols-1 md:grid-cols-3 gap-[0.5rem] gap-y-[2rem] lg:gap-y-[4rem] lg:gap-[2rem] py-[2rem] '
+          >
+            {Array.from({length: 9}).map((_, i) => (
+              <InvestorCard 
+                key={i} 
+                imageUrl='/images/hero1.jpg'
+                name='Emmanuel Michael'
+                company='Chocho Corporation'
+                investmentAmount={400}
+                description='This a lorem description for the investor card component.'
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </MainLayout>
   )
 }
 
-export default Investor;
+export default Investors;
