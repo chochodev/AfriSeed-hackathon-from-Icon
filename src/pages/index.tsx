1a278272ff60bff9717c2b7e36d7a08ba0a6de56
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-09 20:17:58 +0100

    added: businesses page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
 const Investor = lazy(() => import ('./investors'));
-const Business = lazy(() => import ('./businesses'));
+const Business = lazy(() => import ('./business'));
 const Home = lazy(() => import ('./home'));
 
 const Pages = () => {
@@ -12,7 +12,7 @@ const Pages = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/investors' element={<Investor />} />
-          <Route path='/businesses' element={<Business />} />
+          <Route path='/business' element={<Business />} />
         </Routes>
       </Suspense>
     </Router>
