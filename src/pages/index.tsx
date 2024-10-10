653d18b5b0a8147c4d150d6223b36431a34b7456
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-10 03:21:16 +0100

    added: business form page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Suspense, lazy } from 'react';
 const Home = lazy(() => import ('./home'));
 const Investor = lazy(() => import ('./investors'));
 const Business = lazy(() => import ('./business'));
+const BusinessForm = lazy(() => import ('./businessForm'));
 const BusinessPage = lazy(() => import ('./viewBusiness'));
 
 const Pages = () => {
@@ -14,6 +15,7 @@ const Pages = () => {
           <Route path='/' element={<Home />} />
           <Route path='/investors' element={<Investor />} />
           <Route path='/business' element={<Business />} />
+          <Route path='/business/add' element={<BusinessForm />} />
           <Route path='/business/*' element={<BusinessPage />} />
         </Routes>
       </Suspense>
