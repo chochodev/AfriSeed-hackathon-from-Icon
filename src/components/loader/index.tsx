4238a94f5404cf0dca45c0c9378e4d13d480f812
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-12 15:34:55 +0100

    added: loader component

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,20 @@
+
+const Loader = () => {
+  return (
+    <div className='flex place-items-center size-full bg-gradient-to-br from-neutral-500 via-purple-900 to-neutral-900'>
+      <div className="banter-loader">
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader;
