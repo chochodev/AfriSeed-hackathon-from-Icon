b2461a14918a6358ed2fdf6ebb09a5e7f7df1fbd
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-12 16:52:31 +0100

    update: using the business card for the home page businesses

--- src/components/businessCard/index.tsx
+++ src/components/businessCard/index.tsx
@@ -1,13 +1,40 @@
 import { Link } from 'react-router-dom';
 
-const BusinessCard = () => {
+interface Business {
+  id: string
+  logo: string
+  cover_image: string
+
+  name: string
+  short_description: string
+  location: string
+  category: string
+  minimum_investment: number
+  days_left: number
+
+  amount_raised: number
+  investors: number
+
+  // ::::::::::::::: pitch
+  pitch_summary: string
+  pitch_problem: string
+  pitch_solution: string
+  pitch_market_opportunity: string
+  pitch_traction: string
+}
+
+interface BusinessCardProps {
+  business: Business;
+}
+
+const BusinessCard = ({ business }: BusinessCardProps) => {
   return (
     <Link 
-      to='/business/gg'
+      to={`/business/${business.id}`}
       className='group relative flex flex-col h-[22.5rem] lg:h-[31.25rem] w-full rounded-[16px] overflow-hidden shadow-[0_2px_5px_-2px_rgba(0,0,0,0.25)] '
     >
       <img 
-        src="/images/hero1.jpg" 
+        src={`${business.cover_image}`}
         alt="company" 
         className='w-full h-[13rem] lg:h-[15.625rem] rounded-t-[16px] object-cover '
       />
@@ -21,24 +48,24 @@ const BusinessCard = () => {
           className='relative left-[1.5rem] top-[-1.875rem] mb-[-2rem] z-[2] size-[3rem] lg:size-[3.5rem] bg-white p-[5px] rounded-[4px] shadow-[0_1px_5px_-1px_rgba(0,0,0,0.25)] '
         >
           <img
-            src='/images/hero3.jpg'
+            src={`${business.logo}`}
             alt='logo'
             className='size-full object-cover rounded-[4px]'
           />
         </div>
 
         <div className='px-[1.5rem] w-full '>
-          <h2 className='font-[700] text-[1.25rem] lg:text-[1.5rem]'>Business 1</h2>
-          <p className='min-h-[3rem] text-neutral-500 text-[0.75rem] sm:max-md:text-[0.875rem] lg:text-[0.875rem] '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aspernatur delectus optio tempore nostrum.</p>
+          <h2 className='font-[700] text-[1.25rem] lg:text-[1.5rem]'>{business.name}</h2>
+          <p className='min-h-[3rem] text-neutral-500 text-[0.75rem] sm:max-md:text-[0.875rem] lg:text-[0.875rem] '>{business.pitch_summary.slice(0,40)}...</p>
         </div>
 
         {/* :::::::::::::::::::: hidden content on hover */}
         <div 
           className='max-lg:hidden w-full space-y-[0.5rem] px-[1.5rem] translate-y-0 group-hover:translate-y-[15rem] h-[4rem] group-hover:h-0 ease-400 '
         >
-          <p className='text-neutral-400 text-[1rem]'>Lagos, Nigeria</p>
+          <p className='text-neutral-400 text-[1rem]'>{business.location}</p>
           <div className='space-x-[0.5rem]'>
-            <p className='text-neutral-600 bg-neutral-200 text-[0.625rem] w-max rounded-[2px] py-[2px] px-[0.375rem] '>TECH & FINANCE</p>
+            <p className='text-neutral-600 bg-neutral-200 text-[0.625rem] w-max rounded-[2px] py-[2px] px-[0.375rem] '>{business.category}</p>
           </div>
           <small className='text-neutral-400 text-[0.625rem] '>Verified by AfriSeed plc.</small>
         </div>
@@ -48,16 +75,16 @@ const BusinessCard = () => {
           className='h-0 opacity-0 overflow-hidden text-[0.875rem] lg:text-[1rem] pb-[1.5rem] group-hover:h-[5rem] lg:group-hover:h-[10.5rem] group-hover:opacity-100 ease-400 '
         >
           <p className='max-lg:flex pb-2 px-[1.5rem] '>
-            <b className='max-lg:flex-1'>$100,000</b> raised
+            <b className='max-lg:flex-1'>${business.amount_raised}</b> raised
           </p>
           <p className='max-lg:flex border-solid border-0 lg:border-t-[1px] border-neutral-300 py-2 px-[1.5rem] '>
-            <b className='max-lg:flex-1'>122</b> investors
+            <b className='max-lg:flex-1'>{business.investors}</b> investors
           </p>
           <p className='max-lg:hidden border-solid border-0 border-t-[1px] border-neutral-300 py-2 px-[1.5rem] '>
-            <b>$10</b> price per share
+            <b>{business.days_left}</b> days left
           </p>
           <p className='max-lg:hidden border-solid border-0 border-t-[1px] border-neutral-300 py-2 px-[1.5rem] '>
-            <b>$30</b> minimum investment
+            <b>${business.minimum_investment}</b> minimum investment
           </p>
         </div>
       </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import MainLayout from '$/layout';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Skeleton } from '$/components/ui/skeleton';
+import BusinessCard from '$/components/businessCard';
 
 interface Business {
   id: string
@@ -145,67 +146,7 @@ const Home = () => {
           {(businesses.length > 0 )?
             <>
             {businesses.slice(0,3)?.map((business, i) => (
-              <Link 
-                to={`/business/${business.id}`}
-                key={i}
-                className='group relative flex flex-col h-[22.5rem] lg:h-[31.25rem] w-full rounded-[16px] overflow-hidden shadow-[0_2px_5px_-2px_rgba(0,0,0,0.25)] '
-              >
-                <img 
-                  src={`${business.cover_image}`}
-                  alt="company" 
-                  className='w-full h-[13rem] lg:h-[15.625rem] rounded-t-[16px] object-cover '
-                />
-
-                {/* :::::::::::::::::::::: text content */}
-                <div 
-                  className='absolute bottom-0 left-0 z-1 flex flex-1 flex-col h-max w-full space-y-[1rem] bg-white ease-400 delay-100 '
-                >
-                  {/* :::::::::::::::::::::: logo */}
-                  <div
-                    className='relative left-[1.5rem] top-[-1.875rem] mb-[-2rem] z-[2] size-[3rem] lg:size-[3.5rem] bg-white p-[5px] rounded-[4px] shadow-[0_1px_5px_-1px_rgba(0,0,0,0.25)] '
-                  >
-                    <img
-                      src={`${business.logo}`}
-                      alt='logo'
-                      className='size-full object-cover rounded-[4px]'
-                    />
-                  </div>
-
-                  <div className='px-[1.5rem] w-full '>
-                    <h2 className='font-[700] text-[1.25rem] lg:text-[1.5rem]'>{business.name}</h2>
-                    <p className='min-h-[3rem] text-neutral-500 text-[0.75rem] sm:max-md:text-[0.875rem] lg:text-[0.875rem] '>{business.pitch_summary.slice(0,40)}...</p>
-                  </div>
-
-                  {/* :::::::::::::::::::: hidden content on hover */}
-                  <div 
-                    className='max-lg:hidden w-full space-y-[0.5rem] px-[1.5rem] translate-y-0 group-hover:translate-y-[15rem] h-[4rem] group-hover:h-0 ease-400 '
-                  >
-                    <p className='text-neutral-400 text-[1rem]'>Lagos, Nigeria</p>
-                    <div className='space-x-[0.5rem]'>
-                      <p className='text-neutral-600 bg-neutral-200 text-[0.625rem] w-max rounded-[2px] py-[2px] px-[0.375rem] '>TECH & FINANCE</p>
-                    </div>
-                    <small className='text-neutral-400 text-[0.625rem] '>Verified by AfriSeed plc.</small>
-                  </div>
-
-                  {/* :::::::::::::::::::: on hover content */}
-                  <div 
-                    className='h-0 opacity-0 overflow-hidden text-[0.875rem] lg:text-[1rem] pb-[1.5rem] group-hover:h-[5rem] lg:group-hover:h-[10.5rem] group-hover:opacity-100 ease-400 '
-                  >
-                    <p className='max-lg:flex pb-2 px-[1.5rem] '>
-                      <b className='max-lg:flex-1'>$100,000</b> raised
-                    </p>
-                    <p className='max-lg:flex border-solid border-0 lg:border-t-[1px] border-neutral-300 py-2 px-[1.5rem] '>
-                      <b className='max-lg:flex-1'>122</b> investors
-                    </p>
-                    <p className='max-lg:hidden border-solid border-0 border-t-[1px] border-neutral-300 py-2 px-[1.5rem] '>
-                      <b>$10</b> price per share
-                    </p>
-                    <p className='max-lg:hidden border-solid border-0 border-t-[1px] border-neutral-300 py-2 px-[1.5rem] '>
-                      <b>$30</b> minimum investment
-                    </p>
-                  </div>
-                </div>
-              </Link>
+              <BusinessCard business={business} key={i} />
             ))}
             </> :
             (<>
