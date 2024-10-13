630d965b571d21a9ac4897cc10aa4e45a6912176
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-13 18:13:37 +0100

    update: tweaked the view-business page

--- src/pages/viewBusiness/index.tsx
+++ src/pages/viewBusiness/index.tsx
@@ -94,7 +94,7 @@ export default function BusinessPage() {
         </header>
 
         {/* :::::::::::::::::::::::::::::::: Main Content */}
-        <main className="mx-auto px-4 py-8">
+        <main className="max-w-[72rem] mx-auto px-4 py-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             {/* ::::::::::::::::::::::::::::::: Left Column */}
             <div className="lg:col-span-2">
