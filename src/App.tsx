9d2d42fe7e765f8040fcca093f21a73a5a6559de
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-13 17:19:49 +0100

    update: resolved the useWalletContext error by adding the correct provider path and assigned prop activeChain="ethereum" to the provider tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Pages from './pages';
 import './index.css';
-import { ThirdwebProvider } from "thirdweb/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 function App() {
   return (
-    <ThirdwebProvider>
+    <ThirdwebProvider activeChain="ethereum">
       <Pages />
     </ThirdwebProvider>
   )
