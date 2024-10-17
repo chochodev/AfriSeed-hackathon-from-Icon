d2734a773c8e15dcae015ccb75de3fb4d2755c1c
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-17 20:45:44 +0100

    update: folder restructured to encapsulate all business related pages to business folder in the pages dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import { Suspense, lazy } from 'react';
 const Home = lazy(() => import ('./home'));
 const Investor = lazy(() => import ('./investors'));
 const Business = lazy(() => import ('./business'));
-const BusinessForm = lazy(() => import ('./businessForm'));
-const BusinessPage = lazy(() => import ('./viewBusiness'));
-const DeleteBusiness = lazy(() => import ('./delete'));
+const BusinessForm = lazy(() => import ('./business/add'));
+const BusinessPage = lazy(() => import ('./business/view'));
+const DeleteBusiness = lazy(() => import ('./business/delete'));
 const ProfilePage = lazy(() => import ('./profile'));
 const ProtectedRoute = lazy(() => import('./protectedRoute'));
 
