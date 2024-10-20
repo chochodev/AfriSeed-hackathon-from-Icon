555630a7f2ef634f13c4a741dcb2152088ae5035
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-20 20:31:39 +0100

    added: popover

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,40 +3,46 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 // import { defaultTheme } from 'tailwindcss/defaultTheme';
 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx,svelte}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{html,js,jsx,ts,tsx,svelte}"],
   theme: {
-    boxShadow: {
-      'small': '0 2px 5px rgba(0, 0, 0, 0.2)',
-      'inset-glow': 'inset 0 0 0 2px rgba(255, 255, 255, 0.5)',
-    },
-    screens: {
-      'sm': '480px',
-      'smd': '680px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1440px'
-    },
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#e8f7ff',
-          '100': '#d5f0ff',
-          '200': '#b3e1ff',
-          '300': '#85cbff',
-          '400': '#56a6ff',
-          '500': '#2f80ff',
-          '600': '#0c54ff',
-          '700': '#0049ff',
-          '800': '#0642cd',
-          '900': '#103f9f',
-          '950': '#0a235c'
-        },
-
-        secondary: '#F2B49B',
-        tertiary: '#A65E30',
-        ...defaultTheme.colors
-      },
-    }
+  	boxShadow: {
+  		small: '0 2px 5px rgba(0, 0, 0, 0.2)',
+  		'inset-glow': 'inset 0 0 0 2px rgba(255, 255, 255, 0.5)'
+  	},
+  	screens: {
+  		sm: '480px',
+  		smd: '680px',
+  		md: '768px',
+  		lg: '1024px',
+  		xl: '1440px'
+  	},
+  	extend: {
+  		colors: {
+  			primary: {
+  				'100': '#d5f0ff',
+  				'200': '#b3e1ff',
+  				'300': '#85cbff',
+  				'400': '#56a6ff',
+  				'500': '#2f80ff',
+  				'600': '#0c54ff',
+  				'700': '#0049ff',
+  				'800': '#0642cd',
+  				'900': '#103f9f',
+  				'950': '#0a235c',
+  				DEFAULT: '#e8f7ff'
+  			},
+  			secondary: '#F2B49B',
+  			tertiary: '#A65E30',
+  			': '.colors,
+                ...defaultTheme.colors,: '
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
