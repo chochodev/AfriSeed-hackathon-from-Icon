3c87d960dde38061db2f1bc4387a106892446529
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-10-20 17:06:17 +0100

    added: multi-step loader

---
+++ src/components/multiStepLoader/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css'
+
+interface LoaderProps {
+  message: string;
+  status: 'loading' | 'success' | 'error';
+}
+
+const Loader: React.FC<LoaderProps> = ({ message, status }) => {
+  let color = 'text-gray-500';
+  
+  if (status === 'success') {
+    color = 'text-green-500';
+  } else if (status === 'error') {
+    color = 'text-red-500';
+  }
+
+  return (
+    <div className="flex flex-col items-center space-y-4">
+      <div className={`loader-circle ${status === 'loading' ? 'animate-spin' : ''} ${color}`}></div>
+      <p className={`text-lg ${color}`}>{message}</p>
+    </div>
+  );
+};
+
+export default Loader;
